35887a96b516b26084efd6751072cdf546554dd4
Leandro Gabriel Casas <leandrogabriel.casas@gmail.com>
2022-12-17 23:16:09 -0300

    Improving the randomness and parametrization

--- blown-lines/sketch.js
+++ blown-lines/sketch.js
@@ -1,33 +1,52 @@
-let x0, y0, x1, y1;
+let x0, y0, x1, y1, count;
+
+const paddingX = 100;
+const paddingY = 100;
+const divs = 20;
+const noiseHeigth = 200;
 function setup() {
   createCanvas(windowWidth - 10, windowHeight - 14);
 
-  frameRate(60);
+  //frameRate(60);
+  smooth(16);
+  blendMode(MULTIPLY);
 
-  x0 = 100;
-  y0 = 100;
-  x1 = windowWidth - 100;
+  x0 = paddingX;
+  y0 = paddingY;
+  x1 = windowWidth - paddingX;
   y1 = y0;
-
+  count = round(((windowHeight - 14 - paddingY) - y0) / divs);
   background(220);
 }
 
 function draw() {
-  background(220);
+  //background(220);
 
-  stroke(20, 80);
+  stroke(60, 4);
   strokeWeight(1);
-  drawLine(x0, y0, x1, y1);
+  noFill();
+
+  drawLines(x0, y0, x1, y1, count, divs, noiseHeigth);
 }
 
-function drawLine(x0, y0, x1, y1){
-  let r = 5;
-  let rnd = random(-r,r);
-  let x = x0;
-  
-  while(x <= x1){
-    rnd = random(-r,r);
-    line(x0, y0, x++, y1+rnd);
-    x0 = x;
+function drawLines(x0, y0, x1, y1, count, separation, span){
+  let y = y0;
+  for(let i = 0 ; i < count; i++){
+    drawLine(x0, y + i*separation, x1, y+i*separation, span);
   }
+}
+
+function drawLine(x0, y0, x1, y1, h){
+  let t = y0 + frameCount * 0.005;
+
+  beginShape();
+
+  for(let i = x0; i < x1; i++){
+    let r = i / 100;
+    let d = (i / 1000)*(i/1000);
+    let y = y0 + (noise(r, t) - 0.5) * (h * d);
+    vertex (i + random(-0.55,0.55), y);
+  }
+
+  endShape();
 }
