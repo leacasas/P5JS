450ea7b1403b3a606e96c7debea81a3d304c7441
Leandro Gabriel Casas <leandrogabriel.casas@gmail.com>
2022-12-17 22:13:00 -0300

    starting to get something

--- blown-lines/sketch.js
+++ blown-lines/sketch.js
@@ -1,9 +1,8 @@
 let x0, y0, x1, y1;
-
-
 function setup() {
   createCanvas(windowWidth - 10, windowHeight - 14);
-  
+
+  frameRate(60);
 
   x0 = 100;
   y0 = 100;
@@ -16,14 +15,19 @@ function setup() {
 function draw() {
   background(220);
 
-  stroke(20, 200);
+  stroke(20, 80);
   strokeWeight(1);
   drawLine(x0, y0, x1, y1);
 }
 
 function drawLine(x0, y0, x1, y1){
+  let r = 5;
+  let rnd = random(-r,r);
   let x = x0;
+  
   while(x <= x1){
-    line(x0, y0, x++, y1);
+    rnd = random(-r,r);
+    line(x0, y0, x++, y1+rnd);
+    x0 = x;
   }
 }
