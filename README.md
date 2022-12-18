3e80353db87291a1e0f6c6e1d582ffcf69a046c7
Leandro Gabriel Casas <leandrogabriel.casas@gmail.com>
2022-12-18 00:37:00 -0300

    readme + changes to the figure drawing

---
+++ README.md
@@ -0,0 +1,9 @@
+# P5.js concepts
+
+Some p5 sketches where I try to explore some generative ideas
+
+## blown-lines
+
+using perlin noise and linear compositions
+
+![img](/blown-lines/screenshots/blown-lines-1.jpg)
--- blown-lines/sketch.js
+++ blown-lines/sketch.js
@@ -3,11 +3,11 @@ let x0, y0, x1, y1, count;
 const paddingX = 100;
 const paddingY = 100;
 const divs = 20;
-const noiseHeigth = 200;
+const noiseHeigth = 80;
+const ta = 0.01;
 function setup() {
   createCanvas(windowWidth - 10, windowHeight - 14);
 
-  //frameRate(60);
   smooth(16);
   blendMode(MULTIPLY);
 
@@ -27,26 +27,32 @@ function draw() {
   noFill();
 
   drawLines(x0, y0, x1, y1, count, divs, noiseHeigth);
+
 }
 
 function drawLines(x0, y0, x1, y1, count, separation, span){
   let y = y0;
   for(let i = 0 ; i < count; i++){
-    drawLine(x0, y + i*separation, x1, y+i*separation, span);
+    drawLine(x0, y + i*separation, x1, y+i*separation, i, span);
   }
 }
 
-function drawLine(x0, y0, x1, y1, h){
-  let t = y0 + frameCount * 0.005;
+function drawLine(x0, y0, x1, y1, index, h){
+  let fc = frameCount;
+  let t = y0 + fc * ta;
 
   beginShape();
 
-  for(let i = x0; i < x1; i++){
-    let r = i / 100;
-    let d = (i / 1000)*(i/1000);
+  for(let i = x0; i < x1; i+=30){
+    let r = i / 5;
+    let d = (i / 500)*(i/500)*(index/100);
     let y = y0 + (noise(r, t) - 0.5) * (h * d);
-    vertex (i + random(-0.55,0.55), y);
+    curveVertex(i + random(-1,1), y);
   }
 
   endShape();
+}
+
+function mousePressed(){
+  save("test.jpg");
 }
